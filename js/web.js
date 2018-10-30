3bde1101806dc16c5ec5c3bee33884e0e95ca6e7
Adeel <zeeshanadeel26@gmail.com>
2018-10-30 08:21:24 +0500

    axis1.0

---
+++ js/web.js
@@ -0,0 +1,12 @@
+    $(document).ready(function() {
+      var owlh = $("#home-slider");
+
+      // Custom Navigation Events
+      $(document).on('click', '.carousel-arrow-next', function(){
+            owlh.trigger('owl.next');
+      });
+      $(document).on('click', '.carousel-arrow-prev', function(){
+            owlh.trigger('owl.prev');
+      });
+
+    });
